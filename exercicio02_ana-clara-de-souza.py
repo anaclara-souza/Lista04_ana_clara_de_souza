055475144aceadad6388e03b9ca581543e13b739
anaclara-souza <1234anasouza5678@gmail.com>
2025-02-18 20:01:13 -0300

    n

--- exercicio02_ana-clara-de-souza.py
+++ exercicio02_ana-clara-de-souza.py
@@ -1,1 +1,5 @@
 #Ex 02: faça um programa que solicite ao usuário para digitar o seu nome e um número qualquer e em seguida seu nome de acordo com o número que ele digitou.
+nome = input("Digite seu nome: ")
+num = int(input("Digite um numero: "))
+for i in range(num):
+    input(nome)
